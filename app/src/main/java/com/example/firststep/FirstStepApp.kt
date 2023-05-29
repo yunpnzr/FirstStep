68320972c7d5d9abedec1e6ed7df67b0c0f66dfb
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2023-05-29 13:49:49 +0700

    Add file OnBoarding, misahin preview dengan main activity

---
+++ app/src/main/java/com/example/firststep/FirstStepApp.kt
@@ -0,0 +1,20 @@
+package com.example.firststep
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firststep.ui.theme.FirstStepTheme
+
+@Composable
+fun FirstStepApp (
+
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstStepAppPreview(){
+    FirstStepTheme {
+        FirstStepApp()
+    }
+}
---
+++ app/src/main/java/com/example/firststep/ui/onboarding/OnBoarding.kt
@@ -0,0 +1,10 @@
+package com.example.firststep.ui.onboarding
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OnBoarding(
+
+){
+
+}
