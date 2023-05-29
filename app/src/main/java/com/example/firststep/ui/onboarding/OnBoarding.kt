d52648a38ab1f4f6b319df397da976ac36efdbac
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2023-05-29 14:25:46 +0700

    Masih nyusun onboarding

--- app/src/main/java/com/example/firststep/ui/onboarding/OnBoarding.kt
+++ app/src/main/java/com/example/firststep/ui/onboarding/OnBoarding.kt
@@ -1,10 +1,27 @@
 package com.example.firststep.ui.onboarding
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firststep.R
+import com.example.firststep.ui.theme.FirstStepTheme
 
 @Composable
 fun OnBoarding(
-
+    image: Int,
+    text: String
 ){
+    Column(
 
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingPreview(){
+    FirstStepTheme {
+        OnBoarding(R.drawable.onboarding_one,"Welcome to FirstStep!")
+    }
 }
